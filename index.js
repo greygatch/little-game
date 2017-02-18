0d066f170a42f86809727352a72d42a30940f6e1
Evan Gatchell <greygatch@gmail.com>
2017-02-17 20:09:09 -0600

    fin

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 'use strict';
 
-let healthPoints = 100;
-setInterval(damageSimulation, 500);
-
-function damageSimulation() {
+function damageSimulation(healthPoints) {
 
   if (healthPoints > 0) {
     const roll = Math.floor(Math.random() * 10);
@@ -15,20 +12,42 @@ function damageSimulation() {
       const selectedElement = elementalTypes[elementalRoll];
 
 
-      const elementalDmg = parseFloat(.1 * damageAmt).toPrecision(2);
-      const totalDmg = parseFloat(damageAmt + elementalDmg).toPrecision(2);
+      const elementalDmg = parseFloat(.1 * (damageAmt * 2));
+      const totalDmg = parseFloat(damageAmt + elementalDmg);
       console.log('TOTAL:', totalDmg);
 
       healthPoints -= totalDmg;
-      console.log(`TAKING ${damageAmt} POINTS OF DAMAGE AND ${elementalDmg} POINTS OF ${selectedElement} DAMAGE`);
+      console.log(`TAKING ${damageAmt} POINTS OF DAMAGE AND ${elementalDmg.toFixed(2)} POINTS OF ${selectedElement.toUpperCase()} DAMAGE`);
     }
     const remainingHealth = healthPoints > 0 ? healthPoints : 0;
     console.log('****************');
-    console.log(`Remaining Health: ${remainingHealth}%`);
+    console.log(`Remaining Health: ${remainingHealth.toFixed(2)}%`);
     console.log('****************');
   } else {
     console.log('YOU HAVE DIED.');
-    clearInterval(this);
   }
 
 }
+
+function constructFighters() {
+  const fightersArray = [
+    {
+      name: 'Texas Red',
+      healthPoints: 100
+    },
+    {
+      name: 'Ranger',
+      healthPoints: 75
+    }
+  ];
+  return fightersArray;
+}
+
+function beginWarSimulation() {
+  constructFighters().forEach((country, i) => {
+    console.log(`BEGINNING ROUND FOR ${country.name.toUpperCase()}`);
+    damageSimulation(country.healthPoints);
+  })
+}
+
+beginWarSimulation();
